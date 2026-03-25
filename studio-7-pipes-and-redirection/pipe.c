e494abefbf36a1ee6edc3fec6ea1ee8a9567de02
Carter DeLargy <cdelargy1@gmail.com>
2026-03-25 13:06:56 -0500

    finished studio 7

--- studio-7-pipes-and-redirection/pipe.c
+++ studio-7-pipes-and-redirection/pipe.c
@@ -8,11 +8,15 @@ int main() {
     pid_t fork2;
 
     int fd[2];
-    pipe(fd[2]);
+    pipe(fd);
 
     fork1 = fork();
 
     if (fork1 == 0) {
+        dup2(fd[1], STDOUT_FILENO);
+
+        close(fd[0]);
+
         char* cmd = "./program1";
         char* myargv[] = {"program1", '\0'};
         int ret = execvp(cmd, myargv);
@@ -23,6 +27,8 @@ int main() {
         fork2 = fork();
         
         if (fork2 == 0) {
+            dup2(fd[0], STDIN_FILENO);
+            close(fd[1]);
             char* cmd = "./program2";
             char* myargv[] = {"program2", '\0'};
             int ret = execvp(cmd, myargv);
@@ -30,7 +36,10 @@ int main() {
                 perror("Error executing print");
             }
         } else {
-            waitpid(fork1, fork2);
+            close(fd[1]);
+            close(fd[0]);
+            waitpid(fork1, NULL, 0);
+            waitpid(fork2, NULL, 0);    
         }
     }
 }
