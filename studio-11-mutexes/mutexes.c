38787538ee7065a4f22065080573f7c47633b312
Carter DeLargy <cdelargy1@gmail.com>
2026-04-29 17:05:25 -0500

    up to date

--- studio-11-mutexes/mutexes.c
+++ studio-11-mutexes/mutexes.c
@@ -3,28 +3,45 @@
 #include <stdlib.h>
 
 int race = 0;
+pthread_mutex_t mutex;
+
+
+
 
 void* adder(void* arg) {
+    pthread_mutex_lock(&mutex);
     for (int i = 0; i < 20000000; i++) {
         race++;
     }
+    pthread_mutex_unlock(&mutex);
+
 }
 
 void* subtractor(void* arg) {
+    pthread_mutex_lock(&mutex);
     for (int i = 0; i < 20000000; i++) {
         race--;
     }
+    pthread_mutex_unlock(&mutex);
+
 }
 
 int main() {
     pthread_t thread1, thread2;
 
-    if (pthread_create(&thread1, NULL, adder, NULL) != 0) {
+
+
+    if (pthread_mutex_init(&mutex, NULL) != 0) {
+        printf("error setting up mutex");
+        exit(1);
+    }
+
+    if (pthread_create(&thread1, NULL, adder, &mutex) != 0) {
         printf("error with thread 1");
         exit(1);
     }
 
-    if (pthread_create(&thread2, NULL, subtractor, NULL) != 0) {
+    if (pthread_create(&thread2, NULL, subtractor, &mutex) != 0) {
         printf("error with thread 2");
         exit(1);
     }
